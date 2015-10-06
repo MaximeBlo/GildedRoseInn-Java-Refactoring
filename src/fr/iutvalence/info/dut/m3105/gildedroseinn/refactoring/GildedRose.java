81cac8187b7886d6d2f571f3008c96dff8155027
blouin maxime <maxime.blouin@iut-valence.fr>
2015-10-06 13:55:12 +0200

    Commit 3. Add comments on the methods

--- src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
+++ src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
@@ -20,15 +20,17 @@ public class GildedRose
 
 		items = new ArrayList<Item>();
 		items.add(new Item("+5 Dexterity Vest", 10, 20));
-		items.add(new Item("Aged Brie", 2, MIN_QUALITY));
+		items.add(new Item("Aged Brie", 2, 0));
 		items.add(new Item("Elixir of the Mongoose", 5, 7));
-		items.add(new Item("Sulfuras, Hand of Ragnaros", MIN_QUALITY, 80));
+		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
 		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 		items.add(new Item("Conjured Mana Cake", 3, 6));
 
 		updateQuality();
 	}
-
+	/**The method make some modification on items in the store. 
+	 * 
+	 */
 	public static void updateQuality()
 	{
 		for (int i = 0; i < items.size(); i++)
